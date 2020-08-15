ce0859af95443956e7da8096302157d54af9377e
Ian Azevedo <ianbazevedo@gmail.com>
2020-08-14 22:50:45 -0300

    paleta de cores do app

---
+++ src/config/color.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: "#03092d",
+  primaryLight: "#2c3156",
+  primaryDark: "#000005",
+  secondary: "#171a44",
+  secondaryLight: "#434070",
+  secondaryDark: "#00001e",
+  primaryText: "#ffffff",
+  secondaryText: "#ffffff",
+};
